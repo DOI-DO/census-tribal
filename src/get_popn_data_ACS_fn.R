2b72494293a60b5a524c0c72f39f7c388b87627a
Ann Miller <awmiller@ios.doi.gov>
2026-02-18 13:15:13 -0500

    create function for pulling pop estimates by geo and AIAN race response
    New function to pull data from ACS census using tidycensus package

---
+++ src/get_popn_data_ACS_fn.R
@@ -0,0 +1,61 @@
+# This function pulls population totals and AIAN totals (alone, in combination)
+# from 5-year American Community Survey (ACS) data.
+# Geographies = AIANNH home lands
+
+# Result = GEOID, geography name, variable (total popn, AIAN-A or AIAN-IC), estimate, and moe
+
+# other possible geos:
+# "alaska native regional corporation"
+# "american indian area/alaska native area (reservation or statistical entity only)"
+
+# prior to running set key: census_api_key("MY KEY HERE")
+
+# To create a wide table with total, AIAN-A, AIAN-AoIC:
+# pivot_wider(out, id_cols = c("GEOID", "NAME"), names_from = variable, values_from = estimate) %>% 
+#           mutate(AoIC = A + IC) %>% select(GEOID, NAME, total, A, AoIC)
+
+# A Miller, adapted from E Silverman "get_popn_data_DECENNIAL_fn.R"
+
+get_acs_popn_data <- function(year = year,
+                              acs_table = "acs5/profile",
+                              geo = "american indian area/alaska native area/hawaiian home land",
+                              dropNonFed = T) {
+  
+  library(tidycensus)
+  library(tidyverse)
+  
+  # load variables for selected year and table
+  variables <- load_variables(year, acs_table, cache=TRUE)
+  
+  # select all P8 variables with AIAN, as well as the total (_001N)
+  var.select <- c("DP05_0001",
+                  variables %>% 
+                    filter(str_detect(name, "DP05_")) %>%
+                    filter(label == "Estimate!!RACE!!Total population!!One race!!American Indian and Alaska Native" |
+                             label ==  "Estimate!!Race alone or in combination with one or more other races!!Total population!!American Indian and Alaska Native") %>%
+                    select(name) %>% pull)
+  
+  out <- get_acs(geography = geo, 
+                 variables = var.select, 
+                 year = year) %>%
+    left_join(select(variables, name, label), by = c('variable' = 'name')) %>%
+    mutate(variable = ifelse(variable == "DP05_0001", "total",
+                             ifelse(str_detect(label, "One race"), "A", "AoIC"))) %>%
+    select(-label)
+  
+  # To create a wide table with estimates of total, AIAN-A, AIAN-AoIC, uncomment:
+  # out <- pivot_wider(out, id_cols = c("GEOID", "NAME"), names_from = variable, values_from = estimate) %>%
+  #   select(GEOID, NAME, total, A, AoIC)
+  
+  if(dropNonFed) {
+    # drop Native Hawaiian home lands
+    out <- out %>% filter(!(as.numeric(GEOID) %in% c(5000:5499)))
+    # drop state AIR and statistical areas, except 2 Fed recognize:
+    out <- out %>% filter(!(as.numeric(GEOID) > 8999 & 
+                              !(as.numeric(GEOID) %in% c(9260, 9370))))
+    
+  }
+  
+  out
+}
+
